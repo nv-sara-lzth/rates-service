8034a11624de0b6a6e23cab0aa281b62c25fb0ae
Sara Lizbeth Ipiales Delgado <sara.lizbeth.ipiales.delgado@externos.gloval.es>
2025-03-02 17:21:58 +0100

    Add logic for CRUD operations in RateserviceImpl, pending find by multiple filter

--- src/main/java/com/ecommerce/rates_service/services/RateServiceImpl.java
+++ src/main/java/com/ecommerce/rates_service/services/RateServiceImpl.java
@@ -1,15 +1,14 @@
 package com.ecommerce.rates_service.services;
 
 import java.time.LocalDate;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ecommerce.rates_service.dto.RateDTO;
 import com.ecommerce.rates_service.dto.RateResponseDTO;
 import com.ecommerce.rates_service.mappers.RateMapper;
 import com.ecommerce.rates_service.repositories.RateRepository;
+import com.ecommerce.rates_service.utils.RateConstants.OperationDescription;
+import com.ecommerce.rates_service.utils.RateConstants.OperationResult;
 
 import lombok.extern.slf4j.Slf4j;
 import static java.text.MessageFormat.format;
@@ -17,43 +16,90 @@ import static java.text.MessageFormat.format;
 @Service
 @Slf4j
 public class RateServiceImpl implements RateService {
-    
+
     private final RateRepository rateRepository;
     private final RateMapper rateMapper;
 
-    public RateServiceImpl(RateRepository rateRepository, RateMapper rateMapper){
+    public RateServiceImpl(RateRepository rateRepository, RateMapper rateMapper) {
         this.rateRepository = rateRepository;
         this.rateMapper = rateMapper;
     }
-    
+
     @Override
     public RateResponseDTO createRate(RateDTO rateDTO) {
-
         log.info("Creando tarifa...");
 
-        final var rate = rateMapper.toEntity(rateDTO);
-        final var savedRate = rateRepository.save(rate);
+        try {
+            final var rate = rateMapper.toEntity(rateDTO);
+            final var savedRate = rateRepository.save(rate);
+            
+            log.info(format("Tarifa creada con id [{0}]", savedRate.getId()));
 
-        log.info(format("Tarifa creada con id [{0}]", savedRate.getId()));
-        return null;
+            return rateMapper.toResponseDto(null, OperationResult.OK, format(OperationDescription.RATE_CREATED, savedRate.getId()));
+        } catch (Exception e) {
+            log.error(format("Error inesperado creando la tarifa {0}", e.getMessage()));
+            return rateMapper.toResponseDto(null, OperationResult.KO, format(OperationDescription.RATE_GENERIC_ERROR, " creando tarifa"));
+        }
     }
 
     @Override
     public RateResponseDTO findRateById(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findRateById'");
+        log.info(format("Buscando tarifa con id {0}...", id));
+        try {
+            final var rateOptional = rateRepository.findById(id);
+            if (rateOptional.isPresent()) {
+                final var rate = rateOptional.get();
+                log.info(format("Tarifa encontrada con id {0}", id));
+                return rateMapper.toResponseDto(rate, OperationResult.OK, OperationDescription.RATE_FOUND);
+            }else {
+                log.warn(format("No se encontro ninguna tarifa con id {0}", id));
+                return rateMapper.toResponseDto(null, OperationResult.KO, OperationDescription.RATE_NOT_FOUND);
+            }
+        } catch (Exception e) {
+            log.error(format("Error inesperado buscando tarifa con id [{0}] Causa [{1}]", id, e.getMessage()));
+            return rateMapper.toResponseDto(null, OperationResult.KO, format(OperationDescription.RATE_GENERIC_ERROR, "buscando tarifa"));
+        }
     }
 
     @Override
     public RateResponseDTO updateRatePrice(Long id, Integer newPrice) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateRatePrice'");
+        log.info(format("Actualizando precio de tarifa con id {0}...", id));
+        try {
+            final var rateOptional = rateRepository.findById(id);
+            if (rateOptional.isPresent()) {
+                final var rate = rateOptional.get();
+                rate.setPrice(newPrice);
+                rateRepository.save(rate);
+
+                log.info(format("Precio actualizado para tarifa con id {0}", id));
+                return rateMapper.toResponseDto(null, OperationResult.OK, OperationDescription.RATE_UPDATED);  
+            }else{
+                log.warn(format("No se encontro la tarifa con id {0}", id));
+                return rateMapper.toResponseDto(null, OperationResult.KO, OperationDescription.RATE_NOT_FOUND);
+            }
+        } catch (Exception e) {
+            log.error(format("Error inesperado actualizando tarifa con id [{0}] Causa [{1}]", id, e.getMessage()));
+            return rateMapper.toResponseDto(null, OperationResult.KO, format(OperationDescription.RATE_GENERIC_ERROR, "actualizando tarifa"));
+        }
     }
 
     @Override
     public RateResponseDTO deleteRate(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteRate'");
+        log.info(format("Eliminando tarifa con id {0}...", id));
+        try {
+            final var rateOptional = rateRepository.findById(id);
+            if (rateOptional.isPresent()) {
+                rateRepository.deleteById(id);
+                log.info(format("Eliminada tarifa con id {0}", id));
+                return rateMapper.toResponseDto(null, OperationResult.OK, format(OperationDescription.RATE_DELETED,id));                
+            }else {
+                log.warn(format("No se encontro tarifa con id {0}", id));
+                return rateMapper.toResponseDto(null, OperationResult.KO, OperationDescription.RATE_NOT_FOUND);
+            }
+        } catch (Exception e) {
+            log.error(format("Error inesperado eliminando tarifa con id [{0}] Cause [{1}]", id, e.getMessage()));
+            return rateMapper.toResponseDto(null, OperationResult.KO, format(OperationDescription.RATE_GENERIC_ERROR, "eliminando tarifa"));
+        }
     }
 
     @Override
@@ -61,5 +107,5 @@ public class RateServiceImpl implements RateService {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'findRateByBrandProductDate'");
     }
-    
+
 }
