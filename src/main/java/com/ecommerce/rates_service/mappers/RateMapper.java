244634f8b2deda92db5dd93c7ad0a42c23229014
Sara Lizbeth Ipiales Delgado <sara.lizbeth.ipiales.delgado@externos.gloval.es>
2025-03-02 15:25:01 +0100

    Add RateConstants utility for operation responses

--- src/main/java/com/ecommerce/rates_service/mappers/RateMapper.java
+++ src/main/java/com/ecommerce/rates_service/mappers/RateMapper.java
@@ -4,6 +4,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import com.ecommerce.rates_service.dto.RateDTO;
+import com.ecommerce.rates_service.dto.RateResponseDTO;
 import com.ecommerce.rates_service.model.Rate;
 
 @Mapper(componentModel = "spring")
@@ -14,5 +15,13 @@ public interface RateMapper {
 
     @Mapping(target = "price", source = "price")
     Rate toEntity(RateDTO rateDTO);
+
+    default RateResponseDTO toResponseDto(Rate rate, String result, String description) {
+    return RateResponseDTO.builder()
+        .result(result)
+        .rate(rate != null ? toDto(rate) : null)
+        .description(description)
+        .build();
+}
     
 }
---
+++ src/main/java/com/ecommerce/rates_service/utils/RateConstants.java
@@ -0,0 +1,24 @@
+package com.ecommerce.rates_service.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class RateConstants {
+
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class OperationResult{
+        public static final String OK = "OK";
+        public static final String KO = "KO";
+    }
+
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class OperationDescription{
+        public static final String RATE_CREATED = "Tarifa con id {0} creada correctamente";
+        public static final String RATE_UPDATED = "Precio de tarifa actualizado correctamente";
+        public static final String RATE_DELETED = "Tarifa con id {0} eliminada correctamente";
+        public static final String RATE_FOUND = "Tarifa encontrada";
+        public static final String RATE_NOT_FOUND = "Tarifa no encontrada"; 
+    }
+    
+}
